a0ce969015567113d975507888df90366e3bdcb3
Marcelo Raineri <mraineridev@gmail.com>
2025-10-05 00:19:05 -0300

    feat(Aircraft): implement Aircraft class with properties and constructor

--- src/ro/academyplus/avaj/simulator/Aircraft/Aircraft.java
+++ src/ro/academyplus/avaj/simulator/Aircraft/Aircraft.java
@@ -6,10 +6,20 @@
 /*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 15:46:46 by mraineri          #+#    #+#             */
-/*   Updated: 2025/10/04 15:46:47 by mraineri         ###   ########.fr       */
+/*   Updated: 2025/10/05 00:18:34 by mraineri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+import src.ro.academyplus.avaj.simulator.Coordinates;
+
 class Aircraft {
+    private long id;
+    private String name;
+    private Coordinates coordinates;
     
+    private Aircraft(long id, String name, Coordinates coordinates) {
+        this.id = id;
+        this.name = name;
+        this.coordinates = coordinates;
+    }
 }
