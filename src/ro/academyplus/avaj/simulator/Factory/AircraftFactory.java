6da407fa50064321e3aa48f8265e77bb40c982e0
Marcelo Raineri <mraineridev@gmail.com>
2025-10-23 19:29:48 -0300

    refactor: update AircraftFactory class to be abstract and adjust imports

--- src/ro/academyplus/avaj/simulator/Factory/AircraftFactory.java
+++ src/ro/academyplus/avaj/simulator/Factory/AircraftFactory.java
@@ -6,12 +6,15 @@
 /*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 15:46:40 by mraineri          #+#    #+#             */
-/*   Updated: 2025/10/05 00:59:04 by mraineri         ###   ########.fr       */
+/*   Updated: 2025/10/23 19:21:27 by mraineri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package ro.academyplus.avaj.simulator.Factory;
 
-class AircraftFactory {
-    
+import ro.academyplus.avaj.simulator.Flyable;
+import ro.academyplus.avaj.simulator.Coordinates;
+
+abstract class AircraftFactory {
+    protected abstract Flyable createAircraft(long id, String name, Coordinates coordinates);
 }
