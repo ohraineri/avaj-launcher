46df8f35c9279449d218b1a340d5e084e81f904c
Marcelo Raineri <mraineridev@gmail.com>
2025-10-23 18:51:57 -0300

    feat: add execution of Simulator in start.sh after compilation

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 javac -sourcepath src -d classes src/ro/academyplus/avaj/simulator/*.java src/ro/academyplus/avaj/simulator/*/*.java && echo "Compilation successful!"
+java -cp classes ro.academyplus.avaj.simulator.Simulator docs/scenario
