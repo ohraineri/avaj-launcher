d97b5a6a796a5e9f536ae8cc03878deaabe71483
Marcelo Raineri <mraineridev@gmail.com>
2025-10-25 17:08:58 -0300

    feat: add Helicopter class and refactor HelicopterAircraft factory class

--- src/ro/academyplus/avaj/simulator/Aircraft/Helicopter.java
+++ src/ro/academyplus/avaj/simulator/Aircraft/Helicopter.java
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Helicoper.java                                     :+:      :+:    :+:   */
+/*   Helicopter.java                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 15:46:43 by mraineri          #+#    #+#             */
-/*   Updated: 2025/10/23 19:22:18 by mraineri         ###   ########.fr       */
+/*   Updated: 2025/10/25 17:05:38 by mraineri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package ro.academyplus.avaj.simulator.Aircraft;
 import ro.academyplus.avaj.simulator.Coordinates;
 
-public class Helicoper extends Aircraft {
-    public Helicoper(long id, String name, Coordinates coordinates) {
+public class Helicopter extends Aircraft {
+    public Helicopter(long id, String name, Coordinates coordinates) {
         super(id, name, coordinates);
     }
     
---
+++ src/ro/academyplus/avaj/simulator/Factory/HelicopterAircraft.java
@@ -0,0 +1,12 @@
+package ro.academyplus.avaj.simulator.Factory;
+
+import ro.academyplus.avaj.simulator.Coordinates;
+import ro.academyplus.avaj.simulator.Flyable;
+import ro.academyplus.avaj.simulator.Aircraft.Helicopter;
+
+public class HelicopterAircraft extends AircraftFactory {
+    // @Override
+    // public Flyable createAircraft(String p_type, String p_name, Coordinates p_coordinates) {
+    //     return new Helicoper(id, name, coordinates);
+    // }
+}
