a3241dded6a79150da656fd20e0c6bb011a7d0c4
Marcelo Raineri <mraineridev@gmail.com>
2025-11-18 22:42:12 -0300

    refactor: update Tower class to improve observer management and code clarity

--- src/ro/academyplus/avaj/simulator/Weather/Tower.java
+++ src/ro/academyplus/avaj/simulator/Weather/Tower.java
@@ -6,26 +6,29 @@
 /*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 15:46:37 by mraineri          #+#    #+#             */
-/*   Updated: 2025/10/25 12:05:45 by mraineri         ###   ########.fr       */
+/*   Updated: 2025/11/18 22:08:07 by mraineri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package ro.academyplus.avaj.simulator.Weather;
+
+import java.util.ArrayList;
 import java.util.List;
 import ro.academyplus.avaj.simulator.Flyable;
 
 
-class Tower {
-    List<Flyable> observers;
+public class Tower {
+    private List<Flyable> observers = new ArrayList<Flyable>();
+
     public void register(Flyable p_flyable) {
-        
+        this.observers.add(p_flyable);
     }
 
     public void unregister(Flyable p_flyable) {
-        
+        this.observers.remove(p_flyable);
     }
     
     protected void conditionChanged() {
-        // 
+        //
     }
 }
