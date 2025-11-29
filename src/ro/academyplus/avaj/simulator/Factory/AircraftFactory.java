f579fe7b131803d6876df9e0e04b14cfb06094d8
Marcelo Raineri <mraineridev@gmail.com>
2025-11-29 16:26:03 -0300

    refactor: change AircraftFactory to abstract class and implement newAircraft method

--- src/ro/academyplus/avaj/simulator/Factory/AircraftFactory.java
+++ src/ro/academyplus/avaj/simulator/Factory/AircraftFactory.java
@@ -6,7 +6,7 @@
 /*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 15:46:40 by mraineri          #+#    #+#             */
-/*   Updated: 2025/10/25 17:20:40 by mraineri         ###   ########.fr       */
+/*   Updated: 2025/11/21 21:48:20 by mraineri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,27 @@ package ro.academyplus.avaj.simulator.Factory;
 import ro.academyplus.avaj.simulator.Flyable;
 import ro.academyplus.avaj.simulator.Coordinates;
 
-public class AircraftFactory {
+public abstract class AircraftFactory {
+    static long aircraft_id;
+    
     public static Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates) {
-        return AircraftFactoryRegistry.create(p_type, p_name, p_coordinates);
+        AircraftFactory aircraft;
+        switch (p_type) {
+            case "JetPlane":
+                aircraft = new JetPlaneAircraft();
+                break;
+            case "Baloon":
+                aircraft = new BaloonAircraft();
+                break;
+            case "Helicopter":
+                aircraft = new HelicopterAircraft();
+                break;        
+            default:
+                throw new Error("Invalid aircraft type");
+        }        
+        Flyable aircraftInstance = aircraft.create(++aircraft_id, p_name, p_coordinates);
+        return aircraftInstance;
     }
+
+    public abstract Flyable create(long p_id, String p_name, Coordinates p_coordinates);
 }
